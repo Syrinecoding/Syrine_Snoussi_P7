28ade508a1cb279abb41e3e44e28e0af65c021c9
Syrine <sys75@orange.fr>
2022-04-01 16:33:56 +0200

    middleware authComment

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -21,7 +21,7 @@ exports.isAdmin = (req, res, next) => {
 };
 /* Vérifier que l'utilisateur a le droit de supprimer le post (pour l'update aussi ?) :
 admin ou auteur pour route delete Post*/
-exports.deletePost = (req, res, next) => {
+exports.authPost = (req, res, next) => {
     const userIdreq = req.auth.userId;
     const sql = "SELECT isAdmin FROM USERS WHERE userId=?;";
     const sqlParams = [userIdreq];
@@ -58,3 +58,36 @@ exports.deletePost = (req, res, next) => {
     });
 };
 /* Vérifier droit de supprimer un commentaire (pour l'update aussi ?) */
+exports.authComment = (req, res, next) => {
+    const userId = req.auth.userId;
+
+    const sql = "SELECT isAdmin FROM USERS WHERE userId= ?;";
+    const sqlParams = [userId];
+    db.query(sql, sqlParams, (error, result, fields) => {
+        if( error ) {
+            res.status(500).json({ 'error': error.sqlMessage});
+        } else {
+            if(result[0] === 1) {
+                next();
+            } else {
+                const commentId = req.param.commentId;
+                const sql2 = "SELECT userId FROM COMMENTS WHERE commentId = ?;";
+                const sqlParams2 = [commentId];
+                db.query(sql2, sqlParams2, (error, result, fields) => {
+                    if(error) {
+                        res.status(500).json({'error': error.sqlMessage});
+                    } else if (result.length === 0) {
+                        res.status(404).json({ 'error': "Ce commentaire n'existe pas !"});
+                    } else {
+                        const commAuthor = result[0].userId;
+                        if (commAuthor === userId) {
+                            next();
+                        } else {
+                            res.status(403).json({ 'error': 'Accès refusé !'});
+                        }
+                    }
+                });
+            }
+        } 
+    });
+};
